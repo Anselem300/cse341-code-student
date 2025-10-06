fb703669b7a10e711629457a79514ba075fb3fe2
Anselem300 <Osamuel1@byupathway.edu>
2025-10-06 13:10:02 -0700

    Changes made

--- server.js
+++ server.js
@@ -20,7 +20,13 @@ app.use(express.json());
 app.use(morgan('dev'));
 app.use(passport.initialize());
 app.use(cookieParser());
-app.use(cors());
+app.use(
+  cors({
+    origin: '*', // or list your frontend and Swagger origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  })
+);
 
 // routes
 const authRoutes = require('./routes/auth');
